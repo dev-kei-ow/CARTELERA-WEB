6ed79ed48822962931432b13408d96616d7e7e00
ImFre <garciagomezkerwinolivermail.com>
2023-02-01 18:54:06 -0500

    lets go!

---
+++ src/main/java/com/mycomp/WebCarteleraApplication.java
@@ -0,0 +1,13 @@
+package com.mycomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCarteleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCarteleraApplication.class, args);
+	}
+
+}
